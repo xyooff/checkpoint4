af80abcb5b3e82c9737e7e425921315e5d15366c
rodrigue <rodrigue_techer@yahoo.com>
2022-07-20 15:53:51 +0200

    add footer + correction css navbarwebsite

--- frontend/src/components/NavBar/NavBarWebSite.jsx
+++ frontend/src/components/NavBar/NavBarWebSite.jsx
@@ -6,20 +6,24 @@ import logo from "../../assets/logoro.png";
 export default function NavBarWebSite() {
   return (
     <div className="nav-bar-web">
-      <nav className="nav-bar-wibe-site">
-        <img className="logo-nav-bar" src={logo} alt="logo" />
-        <Link className="nav-bar-links" to="/">
-          <p>Acceuil</p>
-        </Link>
-        <Link className="nav-bar-links" to="/competences">
-          <p>Compétences</p>
-        </Link>
-        <Link className="nav-bar-links" to="/portfolio">
-          <p>Portfolio</p>
-        </Link>
-        <Link className="nav-bar-links" to="/contact">
-          <p>Contact</p>
-        </Link>
+      <nav id="navweb" className="nav-bar-wibe-site">
+        <div className="nav-bar-img">
+          <img className="logo-nav-bar" src={logo} alt="logo" />
+        </div>
+        <div className="nav-bar-links-all">
+          <Link className="nav-bar-links" to="/">
+            <p>Acceuil</p>
+          </Link>
+          <Link className="nav-bar-links" to="/competences">
+            <p>Compétences</p>
+          </Link>
+          <Link className="nav-bar-links" to="/portfolio">
+            <p>Portfolio</p>
+          </Link>
+          <Link className="nav-bar-links" to="/contact">
+            <p>Contact</p>
+          </Link>
+        </div>
       </nav>
     </div>
   );
